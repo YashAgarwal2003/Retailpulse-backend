2d091e8fa87ad75eaf791b2bed7eb005b130e5c5
Yash Agarwal <147159613+YashAgarwal2003@users.noreply.github.com>
2025-09-07 04:37:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3000",  # local dev
+    "https://retailpulse-frontend.vercel.app",  # your deployed frontend
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def read_root():
